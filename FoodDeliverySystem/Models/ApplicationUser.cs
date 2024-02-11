1dd76f019afe92b072df66443b3681b823d6647b
jemsdesai123 <jemsdesai23@gmail.com>
2024-02-11 13:30:03 -0500

    Add project files.

---
+++ FoodDeliverySystem/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FoodDeliverySystem.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ FoodDeliverySystem/Models/FoodDeliveryAppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace FoodDeliverySystem.Models
+{
+    public class FoodDeliveryAppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public FoodDeliveryAppDbContext(DbContextOptions<FoodDeliveryAppDbContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            //.OnDelete(DeleteBehavior.Cascade)
+            builder.Entity<ApplicationUser>()
+                                                                .Ignore(u => u.AccessFailedCount);
+            builder.Entity<ApplicationUser>()
+                                            .Ignore(u => u.ConcurrencyStamp);
+            builder.Entity<ApplicationUser>()
+                                            .Ignore(u => u.LockoutEnabled);
+            builder.Entity<ApplicationUser>()
+                                            .Ignore(u => u.LockoutEnd);
+            builder.Entity<ApplicationUser>()
+                                            .Ignore(u => u.NormalizedEmail);
+            builder.Entity<ApplicationUser>()
+                                            .Ignore(u => u.NormalizedUserName);
+            //builder.Entity<ApplicationUser>()
+            //                                .Ignore(u => u.SecurityStamp);     
+            builder.Entity<ApplicationUser>()
+                                            .Ignore(u => u.TwoFactorEnabled);
+            builder.Entity<ApplicationUser>()
+                                            .Ignore(u => u.UserName);
+
+
+        }
+    }
+}
